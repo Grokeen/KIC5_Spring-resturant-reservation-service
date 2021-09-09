9b0833b2f68681e98aa72f9ca531a3255bb77ce9
DESKTOP-P7A5DPI\SEEK <kyungseekyoo@gmail.com>
2021-09-09 16:50:31 +0900

    no message

--- MyProject/src/controller/ReservationController.java
+++ MyProject/src/controller/ReservationController.java
@@ -69,7 +69,32 @@ public class ReservationController extends Action{
 		
 		return "/view/orner/confirm.jsp";
 	}
-	
+	public String info (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		/*
+		 * : 게시물 상세 보기 :board/info?num=41 
+		 * 
+		 * 1. num 파라미터를 이용하여 db에 해당 게시물 조회 
+		 *      Board board = new BoardDao().selectOne(num); 
+		 * 2. 조회수 증가시키기. 
+		 * readcnt+1 new BoardDao().readcntadd(num); 
+		 * 
+		 * 3. 1번에서 조회한 게시물데이터를 화면에 출력하기
+		 */
+
+		int num= Integer.parseInt(request.getParameter("num"));
+		BoardMybatisDao dao = new BoardMybatisDao();
+		Board b = dao.selectOne(num);
+		dao.readcntadd(num);
+		request.setAttribute("board", b);
+		
+		String boardid= (String) request.getSession().getAttribute("boardid");
+		if(boardid == null) {
+			boardid = "1";
+		}
+		request.setAttribute("boardName", getBoardName(boardid));
+		
+		return "/view/board/info.jsp";
+	}
 	
 
 
